b89e720850f90f7b797b99d6148b70105a869b0f
Jerrie-Aries <hidzrie@gmail.com>
2023-07-26 22:55:18 +0000

    [Hotfix] `moderation` plugin v1.3.4:
    - Fix AttributeError when creating new config for a guild.

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -170,7 +170,7 @@ class Moderation(commands.Cog):
         config = self.config_cache.get(guild_id)
         if config is None:
             guild = self.bot.get_guild(int(guild_id))
-            default = ModConfig(self.bot, self.db, guild, data={})
+            default = ModConfig(self, self.db, guild, data={})
             self.config_cache[guild_id] = default
             config = default
         return config
