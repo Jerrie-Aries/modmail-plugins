a5eeb660bd37eb46b3e81e92bd85e3491cc3526b
Jerrie-Aries <hidzrie@gmail.com>
2024-02-02 07:29:04 +0000

    `keepalive` plugin v2.0.0:
    - Remove internal support for UptimeRobot. Users now have to set it manually using dashboard on UptimeRobots website.

--- keepalive/keepalive.py
+++ keepalive/keepalive.py
@@ -10,9 +10,8 @@ import discord
 from discord.ext import commands
 
 from core import checks
-from core.models import getLogger, PermissionLevel
+from core.models import PermissionLevel
 
-from .core.clients import UptimeRobotAPIClient
 from .core.servers import KeepAliveServer
 
 
@@ -27,8 +26,6 @@ __plugin_name__ = __plugin_info__["name"]
 __version__ = __plugin_info__["version"]
 __description__ = "\n".join(__plugin_info__["description"]).format(__plugin_info__["wiki"], __version__)
 
-logger = getLogger(__name__)
-
 
 class KeepAlive(commands.Cog, name=__plugin_name__):
     __doc__ = __description__
@@ -42,7 +39,6 @@ class KeepAlive(commands.Cog, name=__plugin_name__):
         """
         self.bot: ModmailBot = bot
         self.keep_alive: Optional[KeepAliveServer] = None
-        self.uptimerobot_client: Optional[UptimeRobotAPIClient] = None
         self.repl_slug: Optional[str] = os.environ.get("REPL_SLUG")
         self.repl_owner: Optional[str] = os.environ.get("REPL_OWNER")
         self.using_replit: bool = self.repl_slug is not None
@@ -51,13 +47,6 @@ class KeepAlive(commands.Cog, name=__plugin_name__):
         if self.using_replit:
             self.keep_alive = KeepAliveServer(self.repl_slug, self.repl_owner)
             await self.keep_alive.start()
-
-            api_key = os.environ.get("UPTIMEROBOT_API_KEY")
-            if api_key:
-                self.uptimerobot_client = UptimeRobotAPIClient(self, api_key=api_key)
-                await self.uptimerobot_client.check_monitor()
-            else:
-                logger.error("UPTIMEROBOT_API_KEY is not set.")
 
     async def cog_unload(self) -> None:
         await self._shutdown_keep_alive()
@@ -124,41 +113,6 @@ class KeepAlive(commands.Cog, name=__plugin_name__):
         server = self.keep_alive.server
         embed.add_field(name="Server class", value=f"`{str(type(server)).strip('<>')}`")
         embed.set_footer(text=f"Version: v{__version__}")
-        await ctx.send(embed=embed)
-
-    @commands.group(name="uptimerobot", aliases=["uprob"], invoke_without_command=True)
-    @checks.has_permissions(PermissionLevel.OWNER)
-    async def uptimerobot_group(self, ctx: commands.Context):
-        """
-        UptimeRobot tools.
-        """
-        await ctx.send_help(ctx.command)
-
-    @uptimerobot_group.command(name="info")
-    @checks.has_permissions(PermissionLevel.OWNER)
-    async def uptimerobot_info(self, ctx: commands.Context, option: Optional[str] = None):
-        """
-        Shows the UptimeRobot monitor information.
-
-        `option` can be `refresh` or `fetch`, this will fetch the latest monitor information from API.
-        """
-        if self.uptimerobot_client is None:
-            raise commands.BadArgument("UptimeRobot service is not set due to missing API key.")
-        monitor = self.uptimerobot_client.monitor
-        if monitor is None:
-            raise commands.BadArgument("UptimeRobot monitor is not set.")
-
-        if option and option.lower() in ("fetch", "refresh"):
-            await monitor.refresh()
-
-        embed = discord.Embed(title="UptimeRobot", color=self.bot.main_color)
-        embed.add_field(name="ID", value=str(monitor.id))
-        embed.add_field(name="Name", value=monitor.friendly_name)
-        embed.add_field(name="URL", value=monitor.url)
-        embed.add_field(name="Type", value=monitor.type)
-        embed.add_field(name="Interval", value=str(monitor.interval))
-        embed.add_field(name="Status", value=monitor.status)
-
         await ctx.send(embed=embed)
 
 
